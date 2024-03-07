17e71e15a973bccb10d19c7dcba7b38b63593ad6
TanaritDoungsub <tanarit.doun@bumail.net>
2024-03-07 23:09:49 +0700

    Air Resistance Code Success

--- Assets/AirResistance.cs
+++ Assets/AirResistance.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class AirResistance : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Rigidbody rb;
+    public float enginePowerThrust, liftBooster, drag, angularDrag;
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddForce( transform.up * -1 * enginePowerThrust );
+        }
+
+        Vector3 lift = Vector3.Project( rb.velocity , transform.forward );
+        rb.AddForce(transform.up * lift.magnitude * liftBooster);
+
+        rb.drag = rb.velocity.magnitude * drag;
+        rb.angularDrag = rb.velocity.magnitude * angularDrag;
+
+        rb.AddTorque(Input.GetAxis("Horizontal") * transform.forward * -1 );
+
+        rb.AddTorque(Input.GetAxis("Vertical") * transform.right );
     }
 }
